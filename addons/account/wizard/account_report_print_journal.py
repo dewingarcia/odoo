262c58544177131e6e5fe1928ef1377d75841094
pvy <pvy@odoo.com>
2016-01-20 16:49:51 +0100

    [ADD] account: journal report

---
+++ addons/account/wizard/account_report_print_journal.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from openerp import fields, models, _
+from openerp.exceptions import UserError
+
+
+class AccountPrintJournal(models.TransientModel):
+    _inherit = "account.common.journal.report"
+    _name = "account.print.journal"
+    _description = "Account Print Journal"
+
+    sort_selection = fields.Selection([('"account_move_line".date', 'Date'), ('am.name', 'Journal Entry Number'),], 'Entries Sorted by', required=True, default='am.name')
+
+    def _print_report(self, data):
+        data = self.pre_print_report(data)
+        data['form'].update({'sort_selection': self.sort_selection})
+        return self.env['report'].with_context(landscape=True).get_action(self, 'account.report_journal', data=data)
