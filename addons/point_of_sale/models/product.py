a10570f0c60def2f9de741b21466a85059f0d5a0
rap-odoo <rap@odoo.com>
2016-05-10 12:46:13 +0530

    [IMP] point_of_sale: allow product grouping for all uom
    In POS, it is hard-codded in such a way that grouping will be done for the
    uom category that has database id == 1.
    
    We have solved this issue: we have added a new boolean field "Group Products"
    in the uom category form view that will only be visible in developer mode.
    
    If a user chooses this option then products having that uom category will be
    grouped in the POS.
    
    Both uom category Unit and Weight have been set to groupable by default.
    
    =========
    Technical
    =========
     - Removed the hardcoded part of groupable for uom which had database id == 1,
       Now grouping will be done on 'groupable' field of "product.uom", which is
       related to the field of the same name on "product.uom.category".
     - Removed the hardcoded part of is_unit for uom which had database id == 1,
       instead use the name of the uom like it's done elsewhere in the POS...

--- addons/point_of_sale/models/product.py
+++ addons/point_of_sale/models/product.py
@@ -20,3 +20,15 @@ class ProductTemplate(models.Model):
             if self.env['pos.session'].search_count([('state', '!=', 'closed')]):
                 raise UserError(_('You cannot delete a product saleable in point of sale while a session is still opened.'))
         return super(ProductTemplate, self).unlink()
+
+
+class ProductUomCateg(models.Model):
+    _inherit = 'product.uom.categ'
+
+    is_product_groupable = fields.Boolean(string='Group Products in POS', help="Check if you want to group products of this category in Point of Sale")
+
+
+class ProductUom(models.Model):
+    _inherit = 'product.uom'
+
+    is_product_groupable = fields.Boolean(related='category_id.is_product_groupable')
