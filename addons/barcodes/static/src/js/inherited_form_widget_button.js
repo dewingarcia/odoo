e2cb8c71be2f324785ac7ecd2ef70af874a32a8d
keyur gajjar <kga@odoo.com>
2016-06-23 16:02:16 +0530

    [FIX]barcode: regression of 28a9336a

--- addons/barcodes/static/src/js/inherited_form_widget_button.js
+++ addons/barcodes/static/src/js/inherited_form_widget_button.js
@@ -1,9 +1,12 @@
 odoo.define('barcode.InheritedFormWidgetButton', function (require) {
 "use strict";
 
+var core = require('web.core');
 var widgets = require('web.form_widgets');
 var BarcodeEvents = require('barcodes.BarcodeEvents');
 var BarcodeHandlerMixin = require('barcodes.BarcodeHandlerMixin');
+
+var _t = core._t;
 
 // If the button has a barcode_trigger attribute, dynamically inherit
 // BarcodeHandlerMixin and redefine on_barcode_scanned
@@ -15,9 +18,13 @@ var ButtonBarcodeHandlerMixin = _.extend({}, BarcodeHandlerMixin, {
             var self = this;
             this.on_barcode_scanned = function(barcode) {
                 var match = barcode.match(/O-BTN\.(.+)/);
-                if (match && match[1] === self.node.attrs.barcode_trigger &&
-                    (self.$el.is(':visible') || self.$el.parent('.dropdown-menu').length)) {
-                    self.on_click();
+                if (match && match[1] === self.node.attrs.barcode_trigger) {
+                    if (self.$el.is(':visible') || self.$el.not('.o_form_invisible').closest('ul.dropdown-menu').length) {
+                        self.on_click();
+                    } else {
+                        var operation = self.node.attrs.barcode_trigger;
+                        return self.do_warn(_t("Operation Error"), _.str.sprintf(_t("Could not perform operation %s due to button/link not visible on page."), _.str.capitalize(operation)));
+                    }
                 }
             };
         } else {
