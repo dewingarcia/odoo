eb0d63700bf89c9a62bf95535c9f0b5958df26f0
Géry Debongnie <ged@odoo.com>
2016-10-17 16:27:54 +0200

    for qsm

--- odoo/addons/base/ir/ir_qweb/assetsbundle.py
+++ odoo/addons/base/ir/ir_qweb/assetsbundle.py
@@ -104,7 +104,7 @@ class AssetsBundle(object):
         if sep is None:
             sep = '\n            '
         response = []
-        if debug == 'assets':
+        if True:
             if css and self.stylesheets:
                 if not self.is_css_preprocessed():
                     self.preprocess_css(debug=debug)
