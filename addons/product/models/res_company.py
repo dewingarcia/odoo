5060e740e009d28722e42ba2368d9d2712e34602
Denis Vermylen (dve) <dve@odoo.com>
2016-11-16 10:19:57 +0100

    [IMP] product: add displayed weight and volume setting
    You can now configure the displayed UoM for weight and volume in Settings ->
    Configuration, this will be a company-dependent setting.
    You need to already manage multiple UoMs (group_uom, sale/stock setting) to
    have access to the setting.
    
    Weight and Volume widgets will convert it to the chosen UoM.

--- addons/product/models/res_company.py
+++ addons/product/models/res_company.py
@@ -1,11 +1,34 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class ResCompany(models.Model):
     _inherit = "res.company"
+
+    def _default_weight_uom_id(self):
+        kg_uom = self.env.ref('product.product_uom_kgm', raise_if_not_found=False)
+        # necessary check as data not yet loaded on module installation
+        if kg_uom:
+            return kg_uom.id
+
+    def _default_volume_uom_id(self):
+        litre_uom = self.env.ref('product.product_uom_litre', raise_if_not_found=False)
+        # necessary check as data not yet loaded on module installation
+        if litre_uom:
+            return litre_uom.id
+
+    weight_uom_id = fields.Many2one(
+        'product.uom', 'Weight unit of measure',
+        default=_default_weight_uom_id,
+        domain=lambda self: [('category_id', '=', self.env.ref('product.product_uom_categ_kgm').id)],
+        help="This company will display weights in this unit of measure.")
+    volume_uom_id = fields.Many2one(
+        'product.uom', 'Volume unit of measure',
+        default=_default_volume_uom_id,
+        domain=lambda self: [('category_id', '=', self.env.ref('product.product_uom_categ_vol').id)],
+        help="This company will display volumes in this unit of measure.")
 
     @api.model
     def create(self, vals):
