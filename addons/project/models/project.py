4c427f028e66f8a1caa2ec7b92da661a6424f31e
Jérome Maes <jem@openerp.com>
2017-01-18 15:00:32 +0100

    [IMP] project: remove doc count on kanban view
    The document count is removed from kanban view of
    project.project. The count compute method and related
    action are removed too.
    
    In order to keep the kanban card order, it was required
    to adapt all inherited views. The order is 'task', 'issue',
    'rating', 'timesheet', and 'forecast'.

--- addons/project/models/project.py
+++ addons/project/models/project.py
@@ -74,17 +74,6 @@ class Project(models.Model):
         analytic_accounts_to_delete.unlink()
         return res
 
-    def _compute_attached_docs_count(self):
-        Attachment = self.env['ir.attachment']
-        for project in self:
-            project.doc_count = Attachment.search_count([
-                '|',
-                '&',
-                ('res_model', '=', 'project.project'), ('res_id', '=', project.id),
-                '&',
-                ('res_model', '=', 'project.task'), ('res_id', 'in', project.task_ids.ids)
-            ])
-
     def _compute_task_count(self):
         for project in self:
             project.task_count = len(project.task_ids)
@@ -103,30 +92,6 @@ class Project(models.Model):
     def _get_alias_models(self):
         """ Overriden in project_issue to offer more options """
         return [('project.task', "Tasks")]
-
-    @api.multi
-    def attachment_tree_view(self):
-        self.ensure_one()
-        domain = [
-            '|',
-            '&', ('res_model', '=', 'project.project'), ('res_id', 'in', self.ids),
-            '&', ('res_model', '=', 'project.task'), ('res_id', 'in', self.task_ids.ids)]
-        return {
-            'name': _('Attachments'),
-            'domain': domain,
-            'res_model': 'ir.attachment',
-            'type': 'ir.actions.act_window',
-            'view_id': False,
-            'view_mode': 'kanban,tree,form',
-            'view_type': 'form',
-            'help': _('''<p class="oe_view_nocontent_create">
-                        Documents are attached to the tasks and issues of your project.</p><p>
-                        Send messages or log internal notes with attachments to link
-                        documents to your project.
-                    </p>'''),
-            'limit': 80,
-            'context': "{'default_res_model': '%s','default_res_id': %d}" % (self._name, self.id)
-        }
 
     @api.model
     def activate_sample_project(self):
@@ -214,7 +179,6 @@ class Project(models.Model):
                 "- Visible by following customers: employees see everything;\n"
                 "   if website is activated, portal users may see project, tasks or issues followed by\n"
                 "   them or by someone of their company\n")
-    doc_count = fields.Integer(compute='_compute_attached_docs_count', string="Number of documents attached")
     date_start = fields.Date(string='Start Date')
     date = fields.Date(string='Expiration Date', index=True, track_visibility='onchange')
 
