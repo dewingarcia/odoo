e1c0d9446ec353cd5e287c860f58f67e91e791bc
Jigar Patel <jat@openerp.com>
2017-01-20 16:40:36 +0530

    [IMP] website_sale_stock: Add inventory availability - Warning messages:   - Don't show anything   - Show inventory   - Only show below a Threshold   - Show a custom message - Add Default Inventory availability option in website admin under product catalog setting - Show inventory availability message based on variant - Block add to cart if product not available in stock (Block if Show inventory or Only show below a Threshold option is selected)

--- addons/website_sale_stock/models/__init__.py
+++ addons/website_sale_stock/models/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import controllers
-import models
+import product_product
+import product_template
+import website_config_settings
+import sale_order
