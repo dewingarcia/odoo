b9b861e9f0fe573b79f1dd50971bf0b519422166
Dharmraj Jhala <dja@openerp.com>
2017-03-03 15:52:55 +0530

    [IMP] website - Improved binding of Background events - In carousel, target element for background is being changed manually. So for the new target, we have to rebind those events that were bound in start function of 'background' snippet options. For that, code is splitted from start funtion and will be used to rebind necessary events for new target from carousel.

--- addons/website/static/src/js/website.snippets.editor.js
+++ addons/website/static/src/js/website.snippets.editor.js
@@ -232,6 +232,10 @@ options.registry.carousel = options.registry.slider.extend({
                 s_option.$target = self.$target.find(".item.active");
                 s_option.set_active();
                 s_option.$target.trigger("snippet-option-change", [s_option]);
+                if (opt_name == 'background') {
+                    s_option.bind_bg_events();
+                }
+
             });
         });
         this.$target.trigger('slid.bs.carousel');
